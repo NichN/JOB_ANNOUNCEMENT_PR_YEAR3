2c25460b8e2fe21264b63b47de2332218c3492f2
sunnasy <nasy.sunn@gmail.com>
2024-06-02 22:55:36 +0700

    ok

---
+++ js/dropdown_menu_profile.js
@@ -0,0 +1,9 @@
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdown-content");
+    dropdownContent.style.display === "block" ? dropdownContent.style.display = "none" : dropdownContent.style.display = "block";
+}
+
+function toggleSubmenu() {
+    var subDropdownContent = document.getElementById("sub-dropdown-content");
+    subDropdownContent.style.display === "block" ? subDropdownContent.style.display = "none" : subDropdownContent.style.display = "block";
+}
