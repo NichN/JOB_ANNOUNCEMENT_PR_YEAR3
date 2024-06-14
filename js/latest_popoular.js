72493ec5ecc3c7580a09c82ebcf474d4915fbc89
sunnasy <nasy.sunn@gmail.com>
2024-06-14 19:55:44 +0700

    ok

--- js/latest_popoular.js
+++ js/latest_popoular.js
@@ -1,5 +1,5 @@
 const latest_jobs = [
- {
+      {
           "title": "Project Administrator",
           "company": "ProjManage",
           "location": "Denver, CO",
@@ -44,88 +44,56 @@ const latest_jobs = [
               "experience": "3 - 5 years",
               "category": "Information Technology"
           },
-          {
-              "title": "Systems Analyst",
-              "company": "Data Corp",
-              "location": "Dallas, TX",
-              "posted": "5 days ago",
-              "salary": "$75,000 - $95,000",
-              "image": "assets/logos/logo_1.1.png",
-              "description": "Analyze and improve IT systems and processes. Work with stakeholders to gather requirements.",
-              "companyDescription": "Data Corp specializes in providing data-driven solutions for various industries.",
-              "icon": "it.png",
-              "term": "3 year+",
-              "position": "Analyst",
-              "experience": "4 - 6 years",
-              "category": "Information Technology"
-          },
-    {
-        title: "Training and Technical Publication Controller",
-        company: "Training and Technical Publication Controller",
-        location: "Phnom Penh",
-        posted: "2 days ago",
-        salary: "$80,000 - $100,000",
-        image: "assets/logos/com_1.jpeg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.",
-        companyDescription: "ABC Tech is a leading tech company specializing in software development and IT consulting. We offer a dynamic and inclusive work environment.",
-        link: "job_detail.html",
-        icon: "fas fa-laptop-code",
-        category: "Information Technology"
 
-    },
-    {
-        title: "Product Manager",
-        company: "XYZ Inc.",
-        location: "San Francisco, CA",
-        posted: "5 days ago",
-        salary: "$120,000 - $150,000",
-        image: "assets/logos/com_2.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.",
-        companyDescription: "XYZ Inc. is an innovative company at the forefront of the tech industry, known for its creative solutions and strong team culture.",
-        link: "job_detail.html",
-        icon: "fas fa-cogs",
-        category: "Information Technology"
-    },
-        {
-            title: "Product Manager",
-            company: "XYZ Inc.",
-            location: "San Francisco, CA",
-            posted: "5 days ago",
-            salary: "$120,000 - $150,000",
-            image: "assets/logos/com_3.png",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.",
-            companyDescription: "XYZ Inc. is an innovative company at the forefront of the tech industry, known for its creative solutions and strong team culture.",
-            link: "job_detail.html",
-            icon: "fas fa-cogs",
-            category: "Information Technology"
-        }
+
 ];
 
 const popular_jobs = [
-    {
-        title: "Software Engineer",
-        company: "ABC Tech",
-        location: "New York, NY",
-        posted: "2 days ago",
-        salary: "$80,000 - $100,000",
-        image: "assets/logos/logo_1.1.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.",
-        companyDescription: "ABC Tech is a leading tech company specializing in software development and IT consulting. We offer a dynamic and inclusive work environment.",
-        link: "job_detail.html",
-        icon: "fas fa-laptop-code"
-    },
-    {
-        title: "Product Manager",
-        company: "XYZ Inc.",
-        location: "San Francisco, CA",
-        posted: "5 days ago",
-        salary: "$120,000 - $150,000",
-        image: "assets/logos/logo_2.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.",
-        companyDescription: "XYZ Inc. is an innovative company at the forefront of the tech industry, known for its creative solutions and strong team culture.",
-        link: "job_detail.html",
-        icon: "fas fa-cogs"
-    }
+   {
+             "title": "Project Administrator",
+             "company": "ProjManage",
+             "location": "Denver, CO",
+             "posted": "3 days ago",
+             "salary": "$55,000 - $70,000",
+             "image": "assets/logos/logo_1.1.png",
+             "description": "Support project managers in planning and executing projects. Maintain project documentation.",
+             "companyDescription": "ProjManage offers project management and administrative support services.",
+             "icon": "admin.png",
+             "term": "2 year+",
+             "position": "Administrator",
+             "experience": "3 - 5 years",
+             "category": "Administrator"
+         },
+          {
+                 "title": "Software Engineer",
+                 "company": "ABC Tech",
+                 "location": "New York, NY",
+                 "posted": "2 days ago",
+                 "salary": "$80,000 - $100,000",
+                 "image": "assets/logos/logo_1.1.png",
+                 "description": "Develop and maintain software applications. Collaborate with cross-functional teams.",
+                 "companyDescription": "ABC Tech is a leading tech company specializing in software development and IT consulting. We offer a dynamic and inclusive work environment.",
+                 "icon": "it.png",
+                 "term": "3 year+",
+                 "position": "Manager",
+                 "experience": "4 - 6 years",
+                 "category": "Information Technology"
+             },
+             {
+                 "title": "Network Administrator",
+                 "company": "Net Solutions",
+                 "location": "San Jose, CA",
+                 "posted": "1 week ago",
+                 "salary": "$70,000 - $85,000",
+                 "image": "assets/logos/logo_1.1.png",
+                 "description": "Manage and maintain network infrastructure. Ensure network security and performance.",
+                 "companyDescription": "Net Solutions is a leading provider of network services and solutions.",
+                 "icon": "it.png",
+                 "term": "3 year+",
+                 "position": "Administrator",
+                 "experience": "3 - 5 years",
+                 "category": "Information Technology"
+             },
 ];
 
 const latestJobContainer = document.getElementById('latest-job-container');
@@ -139,8 +107,8 @@ latest_jobs.forEach(job => {
     jobCard.innerHTML = `
         <img src="${job.image}" alt="${job.company}">
         <div class="card-content">
-            <h3>${job.title}</h3>
-            <h2><a href="job_detail.html?title=${encodeURIComponent(job.title)}">${job.title}</a></h2>
+
+            <h3><a href="job_detail.html?title=${encodeURIComponent(job.title)}">${job.title}</a></h3>
             <div class="icon-text">
                 <i class="${job.icon}"></i>
                 <p>${job.company}</p>
@@ -175,7 +143,7 @@ popular_jobs.forEach(job => {
     jobCard.innerHTML = `
         <img src="${job.image}" alt="${job.company}">
         <div class="card-content">
-            <h3>${job.title}</h3>
+            <h3><a href="job_detail.html?title=${encodeURIComponent(job.title)}">${job.title}</a></h3>
             <div class="icon-text">
                 <i class="${job.icon}"></i>
                 <p>${job.company}</p>
